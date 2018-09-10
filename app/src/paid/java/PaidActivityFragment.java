8611604f37ffe9cede6ea1a31bd98f4bd57ee849
Aiman <aiman.nabeel@gmail.com>
2018-09-10 14:58:26 +0100

    Step 5: Add a paid flavor

---
+++ app/src/paid/java/PaidActivityFragment.java
@@ -0,0 +1,22 @@
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.udacity.gradle.builditbigger.R;
+
+public class PaidActivityFragment extends Fragment {
+
+    public PaidActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_paid, container, false);
+        return root;
+    }
+}
