95cc0a57a4453d59b08a04f0bdf1bc89e302f9f5
Aiman <aiman.nabeel@gmail.com>
2018-09-11 11:30:26 +0100

    Step 5: Add a paid flavor

--- app/src/paid/java/HomeActivityFragment.java
+++ app/src/paid/java/HomeActivityFragment.java
@@ -1,16 +1,23 @@
 import android.app.Fragment;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 import com.udacity.gradle.builditbigger.R;
 
-public class PaidActivityFragment extends Fragment {
+import java.io.IOException;
 
-    public PaidActivityFragment() {
+
+public class HomeActivityFragment extends Fragment{
+
+    public HomeActivityFragment() {
     }
 
     @Override
